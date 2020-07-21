e6540981f38ebeb0b06c7a58bc64e719d38a1c16
Abraham, Jakob <jakob.abraham@baloise.ch>
2020-07-21 12:40:35 +0200

    added control for searches and queries

--- json-api/controller/appController.js
+++ json-api/controller/appController.js
@@ -6,6 +6,26 @@ exports.getRoot = function(req, res){
     res.sendStatus(200);
 }
 
+exports.postQuery = function(req, res){
+    Model.queryData(req, function(err, data){
+        if(err){
+            res.send(err);
+        } else {
+            res.json(data);
+        }
+    })
+}
+
+exports.postSearch = function(req, res){
+    Model.searchData(function(err, data){
+        if(err){
+            res.send(err);
+        } else {
+            res.json(data);
+        }
+    })
+}
+
 exports.getJson = function(req, res){
     Model.getData(function(err, data){
         if(err){
