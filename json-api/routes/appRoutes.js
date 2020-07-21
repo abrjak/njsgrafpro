ef06e89820db89a182d122119dc7297fc584934b
Abraham, Jakob <jakob.abraham@baloise.ch>
2020-07-21 12:40:18 +0200

    added routes for searches and queries

--- json-api/routes/appRoutes.js
+++ json-api/routes/appRoutes.js
@@ -6,6 +6,12 @@ module.exports = function(app){
     app.route('/')
         .get(ctrl.getRoot);
 
+    app.route('/query')
+        .post(ctrl.postQuery);
+
+    app.route('/search')
+        .post(ctrl.postSearch);
+
     app.route('/json')
         .get(ctrl.getJson)
 }
